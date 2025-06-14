faea21a9774268baf61d3bf5acf980b725c96e3a
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2025-06-14 02:13:43 +0200

    f

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
